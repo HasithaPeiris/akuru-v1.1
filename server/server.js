78986961e65e28ea3c307f9b0ed0f77a3edd1b83
Hasitha Peiris <hasithapeiris.me@gmail.com>
2023-12-19 19:58:39 +0530

    deployment

--- server/server.js
+++ server/server.js
@@ -19,6 +19,19 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/fonts", fontRoute);
 
+// Serve frontend
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, "../", "client", "build", "index.html")
+    )
+  );
+} else {
+  app.get("/", (req, res) => res.send("Please set to production"));
+}
+
 app.listen(process.env.PORT || 5000, () => {
   console.log("Backend server is running!");
 });
